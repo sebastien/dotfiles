4b502abb0cd29fc4a8cec151ffabba0a74f8131e
Sébastien Pierre <sebastien.pierre@nzx.com>
2021-05-08 10:19:26 +1200

    [WIP] Updates

---
+++ base/appenv/projects.appenv.sh
@@ -0,0 +1,3 @@
+appenv_declare projects
+appenv_prepend PYTHONPATH "$HOME/Workspace/cuisine/src"
+appenv_prepend PYTHONPATH "$HOME/Workspace/reporter/src"
--- base/config/bash/config.sh
+++ base/config/bash/config.sh
@@ -3,6 +3,18 @@ BASH_BASE=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 set -o vi
 shopt -s extglob
 
+# Nicer shell experience
+# FROM <https://opensource.com/article/20/3/fish-shell>
+export GREP_OPTIONS="--color=auto"; # make grep colorful
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD; # make ls more colorful as well
+export HISTSIZE=32768; # Larger bash history (allow 32³ entries; default is 500)
+export HISTFILESIZE=$HISTSIZE;
+export HISTCONTROL=ignoredups; # Remove duplicates from history. I use `git status` a lot.
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"; # Make some commands not show up in history
+export LANG="en_US.UTF-8"; # Language formatting is still important
+export LC_ALL="en_US.UTF-8"; # byte-wise sorting and force language for those pesky apps
+export MANPAGER="less -X"; # Less is more
+
 function load-source () {
 	if [ -e "$1" ]; then source "$1"; else echo "Could not source: $1"; fi
 }

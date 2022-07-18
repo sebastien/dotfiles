59cfde3b8984053f5436ff9f5c04254ca57f52ab
Sébastien Pierre <sebastien.pierre@gmail.com>
2022-07-19 11:54:15 +1200

    [Update] env: sink diff

--- base/config/bash/env.sh
+++ base/config/bash/env.sh
@@ -6,5 +6,6 @@ export NPM_PACKAGES="$HOME/.local/share/npm"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 export DENO_INSTALL="$HOME/.deno"
 export BUN_INSTALL="$HOME/.bun"
+export SINK_DIFF="nvim -d"
 export PATH="$DENO_INSTALL/bin:$GOPATH/bin:$NPM_PACKAGES/bin/:$HOME/.cargo/bin:$BUN_INSTALL/bin:$PATH"
 # EOF

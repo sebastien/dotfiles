aea35ce1a92f8d79cd148a0f2ec262f02d5e2b69
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-04-20 15:34:18 +1200

    [Update] secrets: API key

--- base/config/bash/secrets.sh
+++ base/config/bash/secrets.sh
@@ -1,5 +1,6 @@
 SECRETS_PATH=~/.sharedsecrets/
 if [ -e "$SECRETS_PATH" ] && which shared-secrets &> /dev/null ; then
 	export OPENAI_TOKEN="$(shared-secrets get openai.token)"
+	export OPENAI_API_KEY="$OPENAI_TOKEN"
 fi
 # EOF

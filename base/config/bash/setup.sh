53cdb2d42eb33544f86d6e47e220ab60161e86a2
Sébastien Pierre <sebastien.pierre@gmail.com>
2023-08-31 13:41:49 +1200

    [New] git-changes-summary

--- base/config/bash/setup.sh
+++ base/config/bash/setup.sh
@@ -4,11 +4,6 @@ if [ ! -e "$APPENV_DEV_PATH/bin/appenv.bash" ] &&  [ ! -e "$HOME/.local/bin/appe
     curl https://raw.githubusercontent.com/sebastien/appenv/master/install.sh | bash
 fi
 
-# If we have git-town, we install the completions
-if which git-town &> /dev/null; then
-	source <(git-town completions bash)
-fi
-
 # We load nix if we have it
 if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
   . $HOME/.nix-profile/etc/profile.d/nix.sh

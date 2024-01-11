0c71ec873928df0c0b7f0e114982e851b2e26cff
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-01-12 09:13:16 +1300

    [Update] bash: tat supports zellij

--- bin/gnome-backgroud-color.sh
+++ bin/gnome-backgroud-color.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 COLOR="$1"
 if [ -n "$COLOR" ]; then
-echo "Setting color to $COLOR"
-gsettings set org.gnome.desktop.background primary-color "$COLOR"
-gsettings set org.gnome.desktop.background secondary-color "$COLOR"
-gsettings set org.gnome.desktop.background color-shading-type "solid"
+	
+# SEE: https://wiki.archlinux.org/title/GNOME/Tips_and_tricks#Use_custom_colours_and_gradients_for_desktop_background
+	echo "Setting color to $COLOR"
+	gsettings set org.gnome.desktop.background primary-color "$COLOR"
+	gsettings set org.gnome.desktop.background secondary-color "$COLOR"
+	gsettings set org.gnome.desktop.background color-shading-type "solid"
+else
+	echo "$0 COLOR:hex"
 fi
 # EOF

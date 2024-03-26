484a36e09fd6bf7b19a8ced3133c331950bcc38a
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-03-27 08:19:49 +1300

    [Update] env: term multiplexer

--- base/config/bash/env.sh
+++ base/config/bash/env.sh
@@ -8,6 +8,7 @@ export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 export DENO_INSTALL="$HOME/.deno"
 export BUN_INSTALL="$HOME/.bun"
 export SINK_DIFF="nvim -d"
+export TERM_MULTIPLEXER=tmux
 if [ -e "$HOME/Workspace/data" ]; then
 	export DATA_PATH="$HOME/Workspace/data"
 fi

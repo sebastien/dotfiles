f10b7b0c23f40454aab6a9c47476ec76cf517208
Sébastien Pierre <sebastien.pierre@gmail.com>
2025-01-13 12:03:57 +1300

    [Update] bash: mise setup

--- base/config/bash/setup.sh
+++ base/config/bash/setup.sh
@@ -1,7 +1,7 @@
 # Do we have appenv?
 APPENV_DEV_PATH=$HOME/Workspace/Community/appenv
-if [ ! -e "$APPENV_DEV_PATH/bin/appenv.bash" ] &&  [ ! -e "$HOME/.local/bin/appenv.bash" ]; then
-    curl https://raw.githubusercontent.com/sebastien/appenv/master/install.sh | bash
+if [ ! -e "$APPENV_DEV_PATH/bin/appenv.bash" ] && [ ! -e "$HOME/.local/bin/appenv.bash" ]; then
+	curl https://raw.githubusercontent.com/sebastien/appenv/master/install.sh | bash
 fi
 
 if [ ! -d "$HOME/.fzf" ]; then
@@ -10,12 +10,15 @@ if [ ! -d "$HOME/.fzf" ]; then
 	~/.fzf/install
 	echo "DONE!"
 fi
-if [ -e "$HOME/.local/bin/mise" ]; then
-	eval "$(~/.local/bin/mise activate bash)"
+
+APP_MISE="$(which mise 2>/dev/null)"
+if [ -n "$APP_MISE" ]; then
+	eval "$($APP_MISE activate bash)"
 fi
+
 # SEE: https://github.com/lincheney/fzf-tab-completion#bash
 # if [ ! -d "$HOME/.config/bash/fzf-completion.sh" ]; then
-# 	source "$HOME/.config/bash/fzf-completion.sh" 
+# 	source "$HOME/.config/bash/fzf-completion.sh"
 # 	bind -x '"\t": fzf_bash_completion'
 # fi
 #
